82fe41cae36c357ab20a63343233f816dcc623fe
Matthew <mngor@ucsc.edu>
2019-04-30 18:22:28 -0700

    added test cases for email.test.js

--- src/email.test.js
+++ src/email.test.js
@@ -8,3 +8,15 @@ test('banana@ucsc.edu', () => {
 test('sluggy@gmail.com', () => {
   expect(validateEmail('sluggy@gmail.com')).toBeTruthy();
 });
+
+test('banana@ucsc.gov', () => {
+  expect(validateEmail('banana@ucsc.gov')).toBeTruthy();
+});
+
+test('bananaucsc.edu', () => {
+  expect(validateEmail('bananaucsc.edu')).toBeTruthy();
+});
+
+test('bla', () => {
+  expect(validateEmail('bla')).toBeTruthy();
+});
